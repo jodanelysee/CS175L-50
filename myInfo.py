e18ed58d88d778a3bfc0acf48d41012ddab07d56
Jodan Elysee <75323379+jodanelysee@users.noreply.github.com>
2022-01-24 18:59:00 -0500

    myInfo version 2

--- myInfo.py
+++ myInfo.py
@@ -2,4 +2,6 @@
 #Jodan Elysee
 #myInfo
 print('My name is Jodan Elysee')
-print('I am a student')
+print('I live in 171 Joker Avenue, Gotham, NJ 12345')
+print('My phone number is 123-456-7890')
+print('I am majoring in Software Engineering')
